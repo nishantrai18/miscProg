1272162afc429272500f4320e4fed4344a4ede3a
Nishant Rai <nishantr@iitk.ac.in>
2016-06-22 01:50:21 -0700

    Added bagging regressors in fetB features

--- chaLearn/code/fetBRegressors.py
+++ chaLearn/code/fetBRegressors.py
@@ -8,6 +8,8 @@ from sklearn import preprocessing
 from sklearn import linear_model
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import BaggingRegressor
+from sklearn.tree import DecisionTreeRegressor
 
 videoPath = '../training/download_train-val/trainFiles/'
 vidNames = os.listdir(videoPath)
@@ -43,8 +45,9 @@ clfList = []
 # modelChoice = 'NN'	# Ever so slightly better than constant
 # modelChoice = 'SVR'	# Gives out of bounds results
 # modelChoice = 'RF'
-modelChoice = 'LGR'	# very poor results
-# modelChoice = 'MISC'
+# modelChoice = 'LGR'	# very poor results
+modelChoice = 'MISC'
+# modelChoice = 'BAG'
 
 if (modelChoice == 'SVR'):
 	modelName = 'visualFetB_SVR'
@@ -108,10 +111,28 @@ elif (modelChoice == 'MISC'):
 	for i in range(5):
 		print 'Currently training the', i, 'th regressor'
 		# clfList.append(SVR(C = 1.0, kernel = 'rbf'))
-		clfList.append(linear_model.Ridge(alpha = 9500))
+		clfList.append(linear_model.Ridge(alpha = 0.05))
 		clfList[i].fit(X_train, Y_train[:,i])
 		print 'Model Trained. Prediction in progress'
 		Y_pred[:,i] = clfList[i].predict(X_test)
+		print np.corrcoef(Y_pred[:,i], Y_test[:,i])
+
+elif (modelChoice == 'BAG'):
+	modelName = 'visualFetB_BAG'
+	model_file_name = 'tmpData/models/visualFetB_BAG'
+
+	for i in range(5):
+		print 'Currently training the', i, 'th regressor'
+		# clfList.append(SVR(C = 1.0, kernel = 'rbf'))
+		clfList.append(BaggingRegressor(DecisionTreeRegressor(), n_estimators = 40, n_jobs = 4))
+		# clfList.append(BaggingRegressor(linear_model.Ridge(alpha = 5)))		
+		# clfList.append(linear_model.SGDRegressor())
+		clfList[i].fit(X_train, Y_train[:,i])
+		print 'Model Trained. Prediction in progress'
+		Y_pred[:,i] = clfList[i].predict(X_test)
+		print np.max(Y_pred[:,i])
+		print np.min(Y_pred[:,i])
+		print np.mean(Y_pred[:,i])
 		print np.corrcoef(Y_pred[:,i], Y_test[:,i])
 
 elif (modelChoice == 'RF'):
@@ -121,7 +142,7 @@ elif (modelChoice == 'RF'):
 	for i in range(5):
 		print 'Currently training the', i, 'th regressor'
 		# clfList.append(SVR(C = 1.0, kernel = 'rbf'))
-		clfList.append(RandomForestRegressor(random_state=0, n_estimators=91))
+		clfList.append(RandomForestRegressor(random_state=0, n_estimators=11))
 		clfList[i].fit(X_train, Y_train[:,i])
 		print 'Model Trained. Prediction in progress'
 		Y_pred[:,i] = clfList[i].predict(X_test)
@@ -193,8 +214,10 @@ elif (modelChoice == 'NN'):
 
 # Scale transform data, save the scaler for later use
 # Save the trained models and predictions
-# pickle.dump([clfList, scaler], open(model_file_name + '.p', 'wb'))
+pickle.dump(clfList, open(model_file_name + '.p', 'wb'))
 # pickle.dump(Y_pred, open(model_file_name, 'rb'))
+
+# pickle.dump(predDict, open(predFileName + '.p', 'wb'))
 
 print Y_test
 
