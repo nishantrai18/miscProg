adb0205b8e2d41eaeb886b494cb82a43d1eee69e
Nishant Rai <nishantr@iitk.ac.in>
2016-07-29 21:59:53 +0530

    Use one hot region, district, hospital id (1700), instruments as features Part based models (For different regions, instruments and hospitals) (Done using common model) Input types: One hot, numeric (after sorting with number of employees)

--- zs16/code/model.py
+++ zs16/code/model.py
@@ -1,5 +1,8 @@
 from sklearn.linear_model import Ridge
 from reader import *
+from sklearn.utils import shuffle
+from sklearn.metrics import precision_recall_fscore_support
+
 import sklearn
 from sklearn.svm import SVR, LinearSVR
 from sklearn import preprocessing
@@ -12,17 +15,29 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.ensemble import AdaBoostRegressor
 
+import random
+
 def evaluatePred(Y_pred, Y_true):
 	mmse = np.sum((Y_pred - Y_true)**2)/((Y_pred.shape[0]) * (1.0))
 	bmse = Y_true.var()
 	print mmse, bmse
 	print ((bmse - mmse)/bmse)
 
-if __name__ == "__main__":
-	X, Y = getDataXY()
-	encoder = oneHot(X)
-	Xt = encoder.transform(X)
-	print Xt.shape
+def procedureA():
+	# Trains and generates a prediction file
+	# Uses hard heuristic for buy_or_not
+
+	popFlag = True
+	X, Y = getDataXY(currYearFlag = False, popFlag = popFlag)
+	X, Y = shuffle(X, Y, random_state=0)
+
+	if popFlag:
+		encoder = oneHot(X[:, 1:])
+		Xt = encoder.transform(X[:, 1:])
+		Xt = np.hstack((X[:,1].reshape(-1, 1), Xt))
+	else:
+		encoder = oneHot(X)
+		Xt = encoder.transform(X)
 
 	buySet = set()
 	for i in range(X.shape[0]):
@@ -30,23 +45,185 @@ if __name__ == "__main__":
 		buySet.add(tmpTup)
 	# Y_buy = [1] * Xt.shape[0]
 
-	split = 0.75
+	min_max_scaler = preprocessing.MinMaxScaler()
+	Xt = min_max_scaler.fit_transform(Xt)
+
+	split = 0.9
 	X_train, X_test = Xt[:(int(Xt.shape[0]*split)),:], Xt[int(Xt.shape[0]*split):, :]
 	Y_train, Y_test = Y[:(int(Y.shape[0]*split)),:], Y[int(Y.shape[0]*split):, :]
 	Y_train = Y_train.ravel()
 	Y_test = Y_test.ravel()
 
-	clf = Ridge(alpha = 100)
-	# clf = SVR(C = 5.0, kernel = 'poly', degree = 2)
-	# clf = LinearSVR(C = 100.0)
-	# clf = BaggingRegressor(DecisionTreeRegressor(max_depth = 5), n_estimators = 10, n_jobs = 4)
-	# clf = AdaBoostRegressor(DecisionTreeRegressor(max_depth = 5), n_estimators = 100)
+	print X_train.shape
+	print X_test.shape
+
+	# clf = Ridge(alpha = 100)
+	# clf = SVR(C = 10.0, kernel = 'poly', degree = 2)
+	# clf = LinearSVR(C = 1.0)
+	clf = BaggingRegressor(DecisionTreeRegressor(), n_estimators = 125, n_jobs = 4)
+	# clf = AdaBoostRegressor(DecisionTreeRegressor(), n_estimators = 100)
 	# clf = DecisionTreeRegressor()
+	# clf = RandomForestRegressor(random_state = 0, n_estimators = 200, n_jobs = 4)
+	clf.fit(X_train, Y_train.ravel())
+
+	Y_pred = clf.predict(X_test)
+	evaluatePred(Y_pred, Y_test)
+
+	return clf, encoder, min_max_scaler
+	# generatePredFile(buySet, clf, encoder)
+
+def getNegativeSamples(sizeList, popList, buySet, encoder):
+	# Get negative samples for training
+	# HEURISTIC: Only penalize those which are surely going to be erroneous
+	# Choose only 10% of the total possible samples
+
+	negX = []
+	negY = []
+
+	for i in range(1, sizeList[0]):
+		for j in range(1, sizeList[1]):
+			for k in range(1, sizeList[2]):
+				tmpVal = (i, k)
+				if (tmpVal not in buySet):
+					if (random.randint(0, 2) == 1):
+						fetList = [i, -1, j, k]
+						tmpKey = (fetList[0], fetList[2])
+						if (tmpKey in popList):
+							fetList[0] = popList[tmpKey]
+							fetList[1] = 0
+						else:
+							fetList[0] = 0
+							fetList[1] = 1
+						fetList = np.array(fetList)
+						negX.append(fetList)
+
+	negX = np.array(negX)
+	negY = [0] * negX.shape[0]
+	negY = np.array(negY)
+	negY = negY.reshape(-1, 1)
+
+	print negX.shape
+	print negY.shape
+
+	return negX, negY
+
+def procedureB(paramC = 1.0):
+	# Predicting buy_or_not in a better way, logistic regression with weights on samples
 	
+	popFlag = True # DON'T MODIFY, LOTS OF SAMPLES
+
+	X, Y = getDataXY(currYearFlag = True, popFlag = popFlag)
+
+	Y = [1] * X.shape[0]
+	Y = np.array(Y)
+	Y = Y.reshape(-1, 1)
+
+	if popFlag:
+		encoder = oneHot(X[:, 1:])
+		Xt = encoder.transform(X[:, 1:])
+		Xt = np.hstack((X[:,1].reshape(-1, 1), Xt))
+	else:
+		encoder = oneHot(X)
+		Xt = encoder.transform(X)
+
+	tX, tY = getDataXY(currYearFlag = True, popFlag = False)
+	# To get the index of the hospitals
+	buySet = set()
+	for i in range(tX.shape[0]):
+		tmpTup = (tX[i][0], tX[i][2])
+		buySet.add(tmpTup)
+
+	maxList = np.max(tX, axis = 0)
+
+	popList = getHospitalProfile()
+	negX, negY = getNegativeSamples(maxList, popList, buySet, encoder)
+
+	if popFlag:
+		negXt = encoder.transform(negX[:, 1:])
+		negXt = np.hstack((negX[:,1].reshape(-1, 1), negXt))
+	else:
+		negXt = encoder.transform(negX)
+
+	Xt = np.vstack((Xt, negXt))
+
+	print Y.shape
+	print negY.shape
+
+	Y = np.vstack((Y, negY))
+	Xt, Y = shuffle(Xt, Y, random_state = 0)
+
+	print Xt.shape
+	print Y.shape
+
+	min_max_scaler = preprocessing.MinMaxScaler()
+	Xt = min_max_scaler.fit_transform(Xt)
+
+	split = 0.9
+	X_train, X_test = Xt[:(int(Xt.shape[0]*split)),:], Xt[int(Xt.shape[0]*split):, :]
+	Y_train, Y_test = Y[:(int(Y.shape[0]*split)),:], Y[int(Y.shape[0]*split):, :]
+	Y_train = Y_train.ravel()
+	Y_test = Y_test.ravel()
+
+	print X_train.shape
+	print X_test.shape
+
+	# clf = LogisticRegression(C = paramC, class_weight = {0:1, 1:25}, random_state = 0)
+	# clf = Ridge(alpha = 100)
+	# clf = SVR(C = 10.0, kernel = 'poly', degree = 2)
+	# clf = LinearSVR(C = 1.0)
+	clf = BaggingRegressor(LogisticRegression(C = paramC, class_weight = {0:1, 1:25}, random_state = 0), n_estimators = 100, n_jobs = 4)
+	# clf = AdaBoostRegressor(DecisionTreeRegressor(), n_estimators = 100)
+	# clf = DecisionTreeRegressor()
+	# clf = RandomForestRegressor(random_state = 0, n_estimators = 200, n_jobs = 4)
 	clf.fit(X_train, Y_train.ravel())
 
 	Y_pred = clf.predict(X_test)
 
+		# for t in zip(clf.predict_proba(X_test), Y_test):
+		# 	print t
+
+	print clf.score(X_test, Y_test)
+
+	print precision_recall_fscore_support(Y_test, Y_pred, average = 'binary')
+
+	print clf.coef_
+
 	evaluatePred(Y_pred, Y_test)
 
-	generatePredFile(buySet, clf, encoder)
+	# generatePredFile(buySet, clf, encoder)
+	return clf, encoder, min_max_scaler
+
+def procedureC():
+	clfRevenue, encRev, scalerRev = procedureA()
+	clfBuy, encBuy, scalerBuy = procedureB(paramC = 1000.0)
+	generatePredFileC(clfBuy, clfRevenue, encBuy, encRev, scalerBuy, scalerRev)
+
+
+def visualize():
+	# Get information about district, instrument, etc wise information
+
+	district = {}
+
+	X, Y = getDataXY()
+
+	for i in range(X.shape[0]):
+		if (X[i][3] not in district):
+			district[X[i][3]] = []
+		district[X[i][3]].append(Y[i][0])
+
+	print len(district)
+
+	for i in district.keys():
+		print '\n\nVALUES : ', i, '\n\n'
+		print district[i]
+
+if __name__ == "__main__":
+
+	random.seed(0)
+	# For reproducibility
+
+	procedureC()
+
+	# for c in [0.1, 1, 10, 100, 1000, 10000]:
+	# 	procedureB(c)
+	# visualize()
--- zs16/code/reader.py
+++ zs16/code/reader.py
@@ -22,7 +22,35 @@ def formatField(val):
 		val = val.strip('Instrument ')
 	return getInt(val)
 
-def getDataXY():
+def getHospitalProfile():
+	popList = {}
+	with open('../resources/Dataset/HospitalProfiling.csv', 'rb') as csvfile:
+		reader = csv.reader(csvfile, delimiter = ',')
+		next(reader, None)
+		for row in reader:
+			fetList = []
+			for i in range(3):
+				tmpVal = formatField(row[i])
+				if (tmpVal == "IGNORE"):
+					break
+				fetList.append(tmpVal)
+			if (len(fetList) < 3):
+				continue
+			fetList = np.array(fetList)
+			tmpKey = (fetList[0], fetList[1])
+			if (tmpKey in popList):
+				popList[tmpKey] = max(popList[tmpKey], fetList[2])
+			else:
+				popList[tmpKey] = fetList[2]
+	csvfile.close()
+
+	return popList
+
+def getDataXY(currYearFlag = False, popFlag = False):
+
+	if popFlag:
+		popList = getHospitalProfile()
+
 	dataList = []
 	with open('../resources/Dataset/HospitalRevenue.csv', 'rb') as csvfile:
 		reader = csv.reader(csvfile, delimiter = ',')
@@ -36,14 +64,88 @@ def getDataXY():
 				fetList.append(tmpVal)
 			if (len(fetList) < 16):
 				continue
+
+			if (popFlag):
+				tmpKey = (fetList[0], fetList[2])
+				if (tmpKey in popList):
+					fetList[0] = popList[tmpKey]
+					fetList[1] = 0
+				else:
+					# print tmpKey
+					fetList[0] = 100
+					fetList[1] = 1
+					# print 'OMG'
+
 			fetList = np.array(fetList)
 			dataList.append(fetList)
 	csvfile.close()
+	# dataList.extend(dataList)
+	# dataList.extend(dataList)
+	# dataList.extend(dataList)
 	dataList = np.array(dataList)
+
+	if (currYearFlag):
+
+		currDataList = []
+		with open('../resources/Dataset/ProjectedRevenue.csv', 'rb') as csvfile:
+			reader = csv.reader(csvfile, delimiter = ',')
+			next(reader, None)
+			for row in reader:
+				fetList = []
+				for i in range(1):
+					tmpVal = formatField(row[i])
+					if (tmpVal == "IGNORE"):
+						break
+					fetList.append(tmpVal)
+				for i in range(1, 2):
+					fetList.append(-1)
+				for i in range(1, 3):
+					tmpVal = formatField(row[i])
+					if (tmpVal == "IGNORE"):
+						break
+					fetList.append(tmpVal)
+				for i in range(4, 16):
+					fetList.append(-1)
+				for i in range(3, 4):
+					tmpVal = formatField(row[i])
+					if (tmpVal == "IGNORE"):
+						break
+					fetList.append(tmpVal)
+
+				if (len(fetList) != 17):
+					continue
+
+				if (popFlag):
+					tmpKey = (fetList[0], fetList[2])
+					if (tmpKey in popList):
+						fetList[0] = popList[tmpKey]
+						fetList[1] = 0
+					else:
+						# print tmpKey
+						fetList[0] = 100
+						fetList[1] = 1
+						# print 'OMG'
+
+				fetList = np.array(fetList)
+				currDataList.append(fetList)
+		csvfile.close()
+		# currDataList.extend(currDataList)
+		# currDataList.extend(currDataList)
+		# currDataList.extend(currDataList)
+		currDataList = np.array(currDataList)
+
+		print dataList.shape
+		print currDataList.shape
+
+		dataList = np.vstack((dataList, currDataList))
 
 	print dataList.shape
 
-	X = dataList[:,[0,2,3]]
+	if (popFlag):
+		X = dataList[:,[0,1,2,3]]
+	else:
+		X = dataList[:,[0,2,3]]
+
 	# Change this to modify the features chosen
 	Y = dataList[:,-1:]
 
@@ -96,6 +198,82 @@ def generatePredFile(buySet, clf, encoder):
 				buyFlag = 1
 			gtwriter.writerow([fieldList[i][0], fieldList[i][1], fieldList[i][2], buyFlag, buyFlag * int(Y_pred[i])])
 	csvfile.close()
+
+def generatePredFileC(clfBuy, clfRevenue, encBuy, encRev, scalerBuy, scalerRev, popFlag = True):
+	fieldList = []
+	dataList = []
+
+	# print encoder.n_values_
+
+	if popFlag:
+		popList = getHospitalProfile()
+
+	with open('../resources/Dataset/Solution.csv', 'rb') as csvfile:
+		reader = csv.reader(csvfile, delimiter = ',')
+		next(reader, None)
+		for row in reader:
+			fetList = [-1]
+			for i in range(3):
+				tmpVal = formatField(row[i])
+				if (tmpVal == "IGNORE"):
+					break
+				if (tmpVal >= encRev.n_values_[i]):
+					tmpVal = encRev.n_values_[i] - 1
+				fetList.append(tmpVal)
+			if (len(fetList) < 4):
+				continue
+			if (popFlag):
+				tmpKey = (fetList[1], fetList[2])
+				if (tmpKey in popList):
+					fetList[0] = popList[tmpKey]
+					fetList[1] = 0
+				else:
+					# print tmpKey
+					fetList[0] = 100
+					fetList[1] = 1
+
+			fieldList.append(row)
+			fetList = np.array(fetList)
+			dataList.append(fetList)
+
+	csvfile.close()
+
+	X = np.array(dataList)
+	print X
+
+	if popFlag:
+		Xt_rev = encRev.transform(X[:, 1:])
+		Xt_rev = np.hstack((X[:,1].reshape(-1, 1), Xt_rev))
+	else:
+		Xt_rev = encRev.transform(X)
+
+	if popFlag:
+		Xt_buy = encBuy.transform(X[:, 1:])
+		Xt_buy = np.hstack((X[:,1].reshape(-1, 1), Xt_buy))
+	else:
+		Xt_buy = encBuy.transform(X)
+
+	Xt_buy = scalerBuy.transform(Xt_buy)
+	Xt_rev = scalerRev.transform(Xt_rev)
+
+	Y_pred_Revenue = clfRevenue.predict(Xt_rev)
+	Y_pred_Buy = clfBuy.predict_proba(Xt_buy)
+
+	# for t in list(Y_pred_Buy):
+	# 	print t
+
+	with open('../resources/Dataset/SolutionMine.csv', 'wb') as csvfile:
+		gtwriter = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+		gtwriter.writerow(['Hospital_ID', 'District_ID', 'Instrument_ID', 'Buy_or_not', 'Revenue'])
+		for i in range(0, len(fieldList)):
+			buyFlag = Y_pred_Buy[i][1]
+			if (buyFlag > 0.9):
+				buyFlag = 1
+			else:
+				buyFlag = 0
+			gtwriter.writerow([fieldList[i][0], fieldList[i][1], fieldList[i][2], buyFlag, int(buyFlag * int(Y_pred_Revenue[i]))])
+	csvfile.close()
+
 
 def oneHot(X):
 	encoder = OneHotEncoder(sparse = False)
