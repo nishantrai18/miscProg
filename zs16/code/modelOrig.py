a812033660782eaa91824c9dd44264644b1da6ce
Nishant Rai <nishantr@iitk.ac.in>
2016-08-01 07:28:45 +0530

    Final commit for ZS Associates contest

--- zs16/code/modelOrig.py
+++ zs16/code/modelOrig.py
@@ -1,5 +1,5 @@
 from sklearn.linear_model import Ridge
-from reader import *
+from readerOrig import *
 from sklearn.utils import shuffle
 
 import cPickle as pickle
@@ -24,7 +24,7 @@ def evaluatePred(Y_pred, Y_true):
 	print mmse, bmse
 	print ((bmse - mmse)/bmse)
 
-def procedureA():
+def procedureA(goldenFlag = False):
 	# Trains and generates a prediction file
 	# Uses hard heuristic for buy_or_not
 
@@ -47,7 +47,13 @@ def procedureA():
 	# Y_buy = [1] * Xt.shape[0]
 
 	min_max_scaler = preprocessing.MinMaxScaler()
+
 	# Xt = min_max_scaler.fit_transform(Xt)
+
+	if goldenFlag:
+		print Xt.shape
+		Xt = getGoldenX(Xt, 2, 2 + encoder.feature_indices_[1], 2 + encoder.feature_indices_[0], 2 + min(9, encoder.feature_indices_[1]))
+
 
 	split = 0.9
 	X_train, X_test = Xt[:(int(Xt.shape[0]*split)),:], Xt[int(Xt.shape[0]*split):, :]
@@ -73,7 +79,7 @@ def procedureA():
 	return clf, encoder, min_max_scaler
 	# generatePredFile(buySet, clf, encoder)
 
-def getNegativeSamples(sizeList, regionList, popList, buySet, encoder, fineFlag = False):
+def getNegativeSamples(sizeList, popList, buySet, encoder):
 	# Get negative samples for training
 	# HEURISTIC: Only penalize those which are surely going to be erroneous
 	# Choose only 10% of the total possible samples
@@ -84,15 +90,11 @@ def getNegativeSamples(sizeList, regionList, popList, buySet, encoder, fineFlag 
 	print sizeList
 
 	for i in range(1, sizeList[0]):
-		shift = 0
-		if fineFlag:
-			if (i in regionList):
-				shift = regionList[i] * 100
-		for j in range(1 + shift, sizeList[1] + shift):
+		for j in range(1, sizeList[1]):
 			for k in range(1, sizeList[2]):
 				tmpVal = (i, k)
 				if (tmpVal not in buySet):
-					if (random.randint(0, 2) <= 1):
+					if (random.randint(0, 1) <= 1):
 						fetList = [i, -1, j, k]
 						tmpKey = (fetList[0], fetList[2])
 						if (tmpKey in popList):
@@ -105,12 +107,9 @@ def getNegativeSamples(sizeList, regionList, popList, buySet, encoder, fineFlag 
 						negX.append(fetList)
 
 	# negX = np.array(negX, dtype = np.uint8)
-	# negY = [0] * negX.shape[0]
-	# negY = np.array(negY, dtype = np.uint8)
-	# negY = negY.reshape(-1, 1)
-
 	negX = np.array(negX, dtype = np.int)
 	negY = [0] * negX.shape[0]
+	# negY = np.array(negY, dtype = np.uint8)
 	negY = np.array(negY, dtype = np.int)
 	negY = negY.reshape(-1, 1)
 
@@ -119,14 +118,12 @@ def getNegativeSamples(sizeList, regionList, popList, buySet, encoder, fineFlag 
 
 	return negX, negY
 
-def procedureB(paramC = 1.0):
+def procedureB(paramC = 1.0, goldenFlag = False):
 	# Predicting buy_or_not in a better way, logistic regression with weights on samples
 	
 	popFlag = True # DON'T MODIFY, LOTS OF SAMPLES
 
-	X, Y = getDataXY(currYearFlag = True, popFlag = popFlag, fineFlag = False)
-
-	regionList = getHospitalRegion()
+	X, Y = getDataXY(currYearFlag = True, popFlag = popFlag)
 
 	Y = [1] * X.shape[0]
 	Y = np.array(Y)
@@ -140,7 +137,10 @@ def procedureB(paramC = 1.0):
 		encoder = oneHot(X)
 		Xt = encoder.transform(X)
 
-	tX, tY = getDataXY(currYearFlag = True, popFlag = False, fineFlag = False)
+	# Xt = Xt[:10000]
+	# Y = Y[:10000]
+
+	tX, tY = getDataXY(currYearFlag = True, popFlag = False)
 	# To get the index of the hospitals
 	buySet = set()
 	for i in range(tX.shape[0]):
@@ -150,7 +150,7 @@ def procedureB(paramC = 1.0):
 	maxList = np.max(tX, axis = 0)
 
 	popList = getHospitalProfile()
-	negX, negY = getNegativeSamples(maxList, regionList, popList, buySet, encoder, fineFlag = False)
+	negX, negY = getNegativeSamples(maxList, popList, buySet, encoder)
 
 	if popFlag:
 		negXt = encoder.transform(negX[:, 2:])
@@ -163,28 +163,16 @@ def procedureB(paramC = 1.0):
 
 	print encoder.feature_indices_
 
-	# Xt = Xt[:10000]
-	# negXt = negXt[:10000]
-	# Y = Y[:10000]
-	# negY = negY[:10000]
-
-	# Golden features
-	# Use something else instead of n_values_
 	Xt = np.vstack((Xt, negXt))
-	
-	print Xt.shape
-	Xt = getGoldenX(Xt, 2, 2 + encoder.feature_indices_[1], 2 + encoder.feature_indices_[1], 2 + min(encoder.feature_indices_[1] + 9, encoder.feature_indices_[2]))
-	print Xt.shape
-	print (2, 2 + encoder.feature_indices_[1], 2 + encoder.feature_indices_[1], 2 + min(encoder.feature_indices_[1] + 9, encoder.feature_indices_[2]))
-		
-	# Xt = pickle.load(open("trainGolden.p", "rb"))
-	# Xt = Xt.astype(dtype = np.uint8)
-	# pickle.dump(Xt, open("trainGolden.p", "wb"))
-
-	print 'uHH'
 
 	print Y.shape
 	print negY.shape
+
+	if goldenFlag:
+		print Xt.shape
+		Xt = getGoldenX(Xt, 2, 2 + encoder.feature_indices_[1], 2 + encoder.feature_indices_[1], 2 + min(encoder.feature_indices_[1] + 9, encoder.feature_indices_[2]))
+		print (2, 2 + encoder.feature_indices_[1], 2 + encoder.feature_indices_[1], 2 + min(encoder.feature_indices_[1] + 9, encoder.feature_indices_[2]))
+		print Xt.shape
 
 	Y = np.vstack((Y, negY))
 	Xt, Y = shuffle(Xt, Y, random_state = 0)
@@ -207,9 +195,7 @@ def procedureB(paramC = 1.0):
 	# clf = LogisticRegression(C = paramC, class_weight = {0:1, 1:10}, random_state = 0)
 	clf = RandomForestClassifier(class_weight = {0:1, 1:50}, random_state = 0, n_estimators = 100, n_jobs = 4)
 	# clf = RandomForestClassifier(class_weight = 'auto', random_state = 0, n_estimators = 50, n_jobs = 4)
-	# clf = Ridge(alpha = 100)
-	# clf = SVR(C = 10.0, kernel = 'poly', degree = 2)
-	# clf = LinearSVC(C = 1.0, class_weight = {0:1, 1:7}, random_state = 0)
+	# clf = LinearSVC(C = 1.0, class_weight = {0:1, 1:10}, random_state = 0)
 	# clf = BaggingClassifier(LogisticRegression(C = paramC, class_weight = {0:1, 1:10}, random_state = 0), n_estimators = 50, n_jobs = 4)
 	# clf = AdaBoostRegressor(DecisionTreeRegressor(), n_estimators = 100)
 	# clf = DecisionTreeRegressor()
@@ -229,15 +215,107 @@ def procedureB(paramC = 1.0):
 
 	evaluatePred(Y_pred, Y_test)
 
-	getTestSetAcc(clf, encoder, popFlag = popFlag)
+	getTestSetAcc(clf, encoder, popFlag = popFlag, goldenFlag = goldenFlag)
 
 	# generatePredFile(buySet, clf, encoder)
 	return clf, encoder, min_max_scaler
 
+def procedureD(paramC = 1.0):
+	# Predicting buy_or_not using warm starts
+	
+	popFlag = True # DON'T MODIFY, LOTS OF SAMPLES
+
+	X, Y = getDataXY(currYearFlag = False, popFlag = popFlag)
+
+	Y = [1] * X.shape[0]
+	Y = np.array(Y)
+	Y = Y.reshape(-1, 1)
+
+	if popFlag:
+		encoder = oneHot(X[:, 2:])
+		Xt = encoder.transform(X[:, 2:])
+		Xt = np.hstack((X[:,:2], Xt))
+	else:
+		encoder = oneHot(X)
+		Xt = encoder.transform(X)
+
+	tX, tY = getDataXY(currYearFlag = True, popFlag = False)
+	# To get the index of the hospitals
+	buySet = set()
+	for i in range(tX.shape[0]):
+		tmpTup = (tX[i][0], tX[i][2])
+		buySet.add(tmpTup)
+
+	maxList = np.max(tX, axis = 0)
+
+	popList = getHospitalProfile()
+	negX, negY = getNegativeSamples(maxList, popList, buySet, encoder)
+
+	if popFlag:
+		negXt = encoder.transform(negX[:, 2:])
+		negXt = np.hstack((negX[:,:2], negXt))
+	else:
+		negXt = encoder.transform(negX)
+
+	Xt = np.vstack((Xt, negXt))
+	Y = np.vstack((Y, negY))
+	Xt, Y = shuffle(Xt, Y, random_state = 0)
+
+	split = 0.9
+	X_train, X_test = Xt[:(int(Xt.shape[0]*split)),:], Xt[int(Xt.shape[0]*split):, :]
+	Y_train, Y_test = Y[:(int(Y.shape[0]*split)),:], Y[int(Y.shape[0]*split):, :]
+	Y_train = Y_train.ravel()
+	Y_test = Y_test.ravel()
+
+	print X_train.shape
+	print X_test.shape
+
+	# clf = LogisticRegression(C = paramC, class_weight = {0:1, 1:10}, random_state = 0)
+	clf = RandomForestClassifier(class_weight = {0:1, 1:50}, random_state = 0, n_estimators = 50, n_jobs = 6)
+	clf.fit(X_train, Y_train.ravel())
+
+	X, Y = getDataProjection(encoder, popFlag = popFlag)
+
+	Y = [1] * X.shape[0]
+	Y = np.array(Y)
+	Y = Y.reshape(-1, 1)
+
+	if popFlag:
+		Xt = encoder.transform(X[:, 2:])
+		Xt = np.hstack((X[:,:2], Xt))
+	else:
+		encoder = oneHot(X)
+		Xt = encoder.transform(X)
+
+	Xt = np.vstack((Xt, negXt[:50000]))
+	Y = np.vstack((Y, negY[:50000]))
+	Xt, Y = shuffle(Xt, Y, random_state = 0)
+
+	clfNew = RandomForestClassifier(class_weight = {0:1, 1:10}, random_state = 0, warm_start = True, n_estimators = 50, n_jobs = 6)
+	clfNew.fit(Xt, Y.ravel())
+
+	Y_pred = clfNew.predict(X_test)
+
+	# for t in zip(clf.predict_proba(X_test), Y_test):
+	# 	print t
+
+	print clfNew.score(X_test, Y_test)
+	print precision_recall_fscore_support(Y_test, Y_pred, average = 'binary')
+
+	# print clf.coef_
+
+	evaluatePred(Y_pred, Y_test)
+	# getTestSetAcc(clf, encoder, popFlag = popFlag)
+
+	# generatePredFile(buySet, clf, encoder)
+	return clfNew, encoder, None
+
 def procedureC():
-	clfRevenue, encRev, scalerRev = procedureA()
-	clfBuy, encBuy, scalerBuy = procedureB(paramC = 0.1)
-	generatePredFileC(clfBuy, clfRevenue, encBuy, encRev, scalerBuy, scalerRev, popFlag = True, newHeuristic = False, fineFlag = False)
+	goldenFlag = False
+	clfRevenue, encRev, scalerRev = procedureA(goldenFlag = goldenFlag)
+	clfBuy, encBuy, scalerBuy = procedureB(paramC = 0.1, goldenFlag = goldenFlag)
+	# clfBuy, encBuy, scalerBuy = procedureD(paramC = 0.1)
+	generatePredFileC(clfBuy, clfRevenue, encBuy, encRev, scalerBuy, scalerRev, popFlag = True, newHeuristic = False, goldenFlag = goldenFlag)
 
 
 def visualize():
--- zs16/code/readerOrig.py
+++ zs16/code/readerOrig.py
@@ -43,20 +43,6 @@ def getGoldenX(X, leftA, rightA, leftB, rightB):
 	Xn = np.array(Xn, dtype = np.int)
 	return Xn
 
-def getGoldenXFast(X, leftA, rightA, leftB, rightB):
-	# Not fast, hstack requires lot of memory while copying.
-	# Plus parallel computation not available
-
-	for i in range(leftA, rightA):
-		for j in range(leftB, rightB):
-			tmpMat = np.multiply(X[:,i], X[:,j]).reshape(-1, 1)
-			X = np.hstack((X, tmpMat))
-		print '\r', (i*(1.0))/(rightA - leftA + 1),
-		sys.stdout.flush()
-
-	print 'COMPLETED'
-	return X
-
 def getHospitalRegion():
 
 	regionList = {}
@@ -80,7 +66,6 @@ def getHospitalRegion():
 
 	return regionList
 
-
 def getHospitalProfile(fineFlag = False):
 	popList = {}
 	regionList = getHospitalRegion()
@@ -97,13 +82,13 @@ def getHospitalProfile(fineFlag = False):
 				fetList.append(tmpVal)
 			if (len(fetList) < 3):
 				continue
-			fetList = np.array(fetList)
 
 			# Fine tune the district
 			if (fineFlag):
 				if (fetList[0] in regionList):
-					fetList[1] += regionList[fetList[0]] * 100
+					fetList[1] += regionList[fetList[0]] * 100				
 
+			fetList = np.array(fetList)
 			tmpKey = (fetList[0], fetList[1])
 			if (tmpKey in popList):
 				popList[tmpKey] = max(popList[tmpKey], fetList[2])
@@ -118,10 +103,9 @@ def getHospitalProfile(fineFlag = False):
 
 	return popList
 
-def getHospitalSales(fineFlag = False):
+def getHospitalSales():
 
-	popList = getHospitalProfile(fineFlag = fineFlag)
-	regionList = getHospitalRegion()
+	popList = getHospitalProfile()
 
 	saleListDetailed = set()
 	saleListRough = set()
@@ -137,11 +121,6 @@ def getHospitalSales(fineFlag = False):
 				fetList.append(tmpVal)
 			if (len(fetList) < 4):
 				continue
-
-			# Fine tune the district
-			if (fineFlag):
-				if (fetList[0] in regionList):
-					fetList[2] += regionList[fetList[0]] * 100				
 
 			tmpKey = (fetList[0], fetList[2])
 			if (tmpKey in popList):
@@ -161,11 +140,10 @@ def getHospitalSales(fineFlag = False):
 
 	return saleListDetailed, saleListRough
 
-def getTestSetAcc(clf, enc, popFlag = True, fineFlag = False):
+def getTestSetAcc(clf, enc, popFlag = True, goldenFlag = False):
 
 	if popFlag:
-		popList = getHospitalProfile(fineFlag = fineFlag)
-	regionList = getHospitalRegion()
+		popList = getHospitalProfile()
 
 	dataList = []
 	with open('../resources/Dataset/ProjectedRevenue.csv', 'rb') as csvfile:
@@ -183,10 +161,6 @@ def getTestSetAcc(clf, enc, popFlag = True, fineFlag = False):
 				fetList.append(tmpVal)
 			if (len(fetList) < 4):
 				continue
-			# Fine tune the district
-			if (fineFlag):
-				if (fetList[0] in regionList):
-					fetList[2] += regionList[fetList[1]] * 100
 
 			if (popFlag):
 				tmpKey = (fetList[1], fetList[2])
@@ -211,9 +185,12 @@ def getTestSetAcc(clf, enc, popFlag = True, fineFlag = False):
 	else:
 		Xt = enc.transform(X)
 
-	# Xt = scaler.transform(Xt)
+	if goldenFlag:
+		print Xt.shape
+		Xt = getGoldenX(Xt, 2, 2 + enc.feature_indices_[1], 2 + enc.feature_indices_[0], 2 + min(enc.feature_indices_[0] + 9, enc.feature_indices_[1]))
+		print Xt.shape
 
-	Xt = getGoldenX(Xt, 2, 2 + enc.feature_indices_[1], 2 + enc.feature_indices_[1], 2 + min(enc.feature_indices_[1] + 9, enc.feature_indices_[2]))
+	# Xt = scaler.transform(Xt)
 
 	Y_pred = clf.predict(Xt)
 
@@ -229,11 +206,10 @@ def getTestSetAcc(clf, enc, popFlag = True, fineFlag = False):
 
 	return Xt
 
-def getDataXY(currYearFlag = False, popFlag = False, fineFlag = False):
+def getDataXY(currYearFlag = False, popFlag = False):
 
 	if popFlag:
-		popList = getHospitalProfile(fineFlag = fineFlag)
-	regionList = getHospitalRegion()
+		popList = getHospitalProfile()
 
 	dataList = []
 	with open('../resources/Dataset/HospitalRevenue.csv', 'rb') as csvfile:
@@ -248,11 +224,6 @@ def getDataXY(currYearFlag = False, popFlag = False, fineFlag = False):
 				fetList.append(tmpVal)
 			if (len(fetList) < 17):
 				continue
-
-			# Fine tune the district
-			if (fineFlag):
-				if (fetList[0] in regionList):
-					fetList[2] += regionList[fetList[0]] * 100
 
 			if (popFlag):
 				tmpKey = (fetList[0], fetList[2])
@@ -304,11 +275,6 @@ def getDataXY(currYearFlag = False, popFlag = False, fineFlag = False):
 				if (len(fetList) < 17):
 					continue
 
-				# Fine tune the district
-				if (fineFlag):
-					if (fetList[0] in regionList):
-						fetList[2] += regionList[fetList[0]] * 100
-
 				if (popFlag):
 					tmpKey = (fetList[0], fetList[2])
 					if (tmpKey in popList):
@@ -352,11 +318,10 @@ def getDataXY(currYearFlag = False, popFlag = False, fineFlag = False):
 
 	return X, Y
 
-def getDataProjection(popFlag = False, fineFlag = False):
+def getDataProjection(encoder, popFlag = False):
 
 	if popFlag:
-		popList = getHospitalProfile(fineFlag = fineFlag)
-	regionList = getHospitalRegion()
+		popList = getHospitalProfile()
 
 	currDataList = []
 	with open('../resources/Dataset/ProjectedRevenue.csv', 'rb') as csvfile:
@@ -371,13 +336,17 @@ def getDataProjection(popFlag = False, fineFlag = False):
 				fetList.append(tmpVal)
 			for i in range(1, 2):
 				fetList.append(-1)
+
 			for i in range(1, 3):
 				tmpVal = formatField(row[i])
 				if (tmpVal == "IGNORE"):
 					break
+				if (tmpVal >= encoder.n_values_[i - 1]):
+					tmpVal = encoder.n_values_[i - 1] - 1
 				fetList.append(tmpVal)
 			for i in range(4, 16):
 				fetList.append(-1)
+
 			for i in range(3, 4):
 				tmpVal = formatField(row[i])
 				if (tmpVal == "IGNORE"):
@@ -387,28 +356,18 @@ def getDataProjection(popFlag = False, fineFlag = False):
 			if (len(fetList) < 17):
 				continue
 
-			# Fine tune the district
-			if (fineFlag):
-				if (fetList[0] in regionList):
-					fetList[2] += regionList[fetList[0]] * 100
-
 			if (popFlag):
 				tmpKey = (fetList[0], fetList[2])
 				if (tmpKey in popList):
 					fetList[0] = popList[tmpKey]
 					fetList[1] = 0
 				else:
-					# print tmpKey
 					fetList[0] = 0
 					fetList[1] = 1
-					# print 'OMG'
 
 			fetList = np.array(fetList)
 			currDataList.append(fetList)
 	csvfile.close()
-	# currDataList.extend(currDataList)
-	# currDataList.extend(currDataList)
-	# currDataList.extend(currDataList)
 	currDataList = np.array(currDataList)
 
 	dataList = np.array(currDataList, dtype = np.int)
@@ -428,52 +387,7 @@ def getDataProjection(popFlag = False, fineFlag = False):
 
 	return X, Y
 
-def generatePredFile(buySet, clf, encoder):
-	fieldList = []
-	dataList = []
-
-	print encoder.n_values_
-
-	with open('../resources/Dataset/Solution.csv', 'rb') as csvfile:
-		reader = csv.reader(csvfile, delimiter = ',')
-		next(reader, None)
-		for row in reader:
-			fetList = []
-			for i in range(3):
-				tmpVal = formatField(row[i])
-				if (tmpVal == "IGNORE"):
-					break
-				if (tmpVal >= encoder.n_values_[i]):
-					tmpVal = encoder.n_values_[i] - 1
-				fetList.append(tmpVal)
-			if (len(fetList) < 3):
-				continue
-			fieldList.append(row)
-			fetList = np.array(fetList)
-			dataList.append(fetList)
-	csvfile.close()
-
-	X = np.array(dataList)
-	print X
-	Xt = encoder.transform(X)
-	Y_pred = clf.predict(Xt)
-	# Y_pred_Revenue = clfRevenue.predict(Xt)
-	# Y_pred_Buy = clfBuy.predict(Xt)
-
-	with open('../resources/Dataset/SolutionMine.csv', 'wb') as csvfile:
-		gtwriter = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
-		gtwriter.writerow(['Hospital_ID', 'District_ID', 'Instrument_ID', 'Buy_or_not', 'Revenue'])
-		for i in range(0, len(fieldList)):
-			tmpTup = (X[i][0], X[i][2])
-			buyFlag = 0
-			# print tmpTup
-			if (tmpTup in buySet):
-				# print "GOOD"
-				buyFlag = 1
-			gtwriter.writerow([fieldList[i][0], fieldList[i][1], fieldList[i][2], buyFlag, ((i%2) or (buyFlag)) * int(Y_pred[i])])
-	csvfile.close()
-
-def generatePredFileC(clfBuy, clfRevenue, encBuy, encRev, scalerBuy, scalerRev, popFlag = True, newHeuristic = False, fineFlag = False):
+def generatePredFileC(clfBuy, clfRevenue, encBuy, encRev, scalerBuy, scalerRev, popFlag = True, newHeuristic = False, goldenFlag = False):
 	fieldList = []
 	dataList = []
 
@@ -482,8 +396,7 @@ def generatePredFileC(clfBuy, clfRevenue, encBuy, encRev, scalerBuy, scalerRev, 
 	print encRev.n_values_
 
 	if popFlag:
-		popList = getHospitalProfile(fineFlag = fineFlag)
-	regionList = getHospitalRegion()
+		popList = getHospitalProfile()
 
 	if newHeuristic:
 		saleListDetailed, saleListRough = getHospitalSales()
@@ -498,15 +411,13 @@ def generatePredFileC(clfBuy, clfRevenue, encBuy, encRev, scalerBuy, scalerRev, 
 				if (tmpVal == "IGNORE"):
 					break
 				if (i > 0):
+					if (tmpVal >= encBuy.n_values_[i - 1]):
+						tmpVal = encBuy.n_values_[i - 1] - 1
 					if (tmpVal >= encRev.n_values_[i - 1]):
 						tmpVal = encRev.n_values_[i - 1] - 1
 				fetList.append(tmpVal)
 			if (len(fetList) < 4):
 				continue
-			# Fine tune the district
-			if (fineFlag):
-				if (fetList[1] in regionList):
-					fetList[2] += regionList[fetList[1]] * 100
 
 			if (popFlag):
 				tmpKey = (fetList[1], fetList[2])
@@ -544,16 +455,24 @@ def generatePredFileC(clfBuy, clfRevenue, encBuy, encRev, scalerBuy, scalerRev, 
 
 	# Xt_buy = Xt_buy[:10000]
 
-	Xt_buy = getGoldenX(Xt_buy, 2, 2 + encBuy.feature_indices_[1], 2 + encBuy.feature_indices_[1], 2 + min(encBuy.feature_indices_[1] + 9, encBuy.feature_indices_[2]))
+	if goldenFlag:
+		print Xt_buy.shape
+		Xt_buy = getGoldenX(Xt_buy, 2, 2 + encBuy.feature_indices_[1], 2 + encBuy.feature_indices_[0], 2 + min(encBuy.feature_indices_[0] + 9, encBuy.feature_indices_[1]))
+		print Xt_buy.shape
 
 	Y_pred_Revenue = clfRevenue.predict(Xt_rev)
+
+	# Xt_buy = Xt_buy[:10000]
+
+	print Xt_buy.shape
+
 	Y_pred_Buy = clfBuy.predict_proba(Xt_buy)
 	# Y_pred_Buy = clfBuy.predict(Xt_buy)
 
 	# for t in list(Y_pred_Buy)[:100]:
 	# 	print t
 
-	with open('../resources/Dataset/SolutionMine.csv', 'wb') as csvfile:
+	with open('../resources/Dataset/SolutionMineAltOrig.csv', 'wb') as csvfile:
 		gtwriter = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
 		gtwriter.writerow(['Hospital_ID', 'District_ID', 'Instrument_ID', 'Buy_or_not', 'Revenue'])
 		for i in range(0, len(fieldList)):
